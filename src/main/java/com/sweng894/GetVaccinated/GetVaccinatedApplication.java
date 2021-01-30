6f19adfe37cc244890c9809b49d83cef5ad5f7fa
Ray Solomon <raybsolomon@gmail.com>
2021-01-30 14:08:42 -0800

    initial commit

---
+++ src/main/java/com/sweng894/GetVaccinated/GetVaccinatedApplication.java
@@ -0,0 +1,13 @@
+package com.sweng894.GetVaccinated;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GetVaccinatedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GetVaccinatedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sweng894/GetVaccinated/GetVaccinatedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sweng894.GetVaccinated;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GetVaccinatedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
