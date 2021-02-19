abdd4dfb7f0437922194f4f0342fd7900168c84a
robynimac229 <robynimac229@outlook.com>
2021-02-18 16:38:46 -0800

    Add files via upload

---
+++ ConfirmationInfoTest.java
@@ -0,0 +1,34 @@
+package com.sweng894.GetVaccinated;
+//SWEN-39 testing for use case SWEN-24
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ConfirmationInfoTest {
+
+  @BeforeEach
+  void setUp() {
+  }
+
+  @AfterEach
+  void tearDown() {
+  }
+
+  @Test
+  void setEmail() {
+  }
+
+  @Test
+  void getEmail() {
+  }
+
+  @Test
+  void setConfirmationNumber() {
+  }
+
+  @Test
+  void getConfirmationNumber() {
+  }
+}
