4efedc3461386b9d892528a0578c3ba5a9253827
Ray Solomon <raybsolomon@gmail.com>
2021-02-23 07:49:53 -0800

    SWEN-18: link to schedule page from home page

--- src/main/java/com/sweng894/GetVaccinated/home/HomeController.java
+++ src/main/java/com/sweng894/GetVaccinated/home/HomeController.java
@@ -1,16 +1,13 @@
 package com.sweng894.GetVaccinated.home;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HomeController {
 
   @GetMapping("/")
-  public String index(@RequestParam(value = "name", defaultValue = "World") String name, Model model) {
-    model.addAttribute("message", name);
+  public String index() {
     return "home";
   }
 }
