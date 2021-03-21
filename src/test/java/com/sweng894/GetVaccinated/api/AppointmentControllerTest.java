97d174dd00562b2e0064503e79663ccb031f86a2
Ray Solomon <raybsolomon@gmail.com>
2021-03-21 13:08:37 -0700

    fix AppointmentController tests running in IntelliJ
    The tests were modified to use an actual Appointment repository instead
    of a mocked one. jmockit was removed as a dependency.

--- src/test/java/com/sweng894/GetVaccinated/api/AppointmentControllerTest.java
+++ src/test/java/com/sweng894/GetVaccinated/api/AppointmentControllerTest.java
@@ -1,34 +1,22 @@
 package com.sweng894.GetVaccinated.api;
 
-import mockit.Expectations;
-import mockit.Injectable;
-import mockit.Mocked;
-import mockit.Tested;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import com.sweng894.GetVaccinated.api.controller.AppointmentController;
-import com.sweng894.GetVaccinated.api.repository.AppointmentRepository;
 import com.sweng894.GetVaccinated.api.entity.Appointment;
 
 @Tag("integration")
 @SpringBootTest
 public class AppointmentControllerTest {
-  @Tested
+  @Autowired
   private AppointmentController appointmentController;
-
-  @Injectable
-  AppointmentRepository repository;
 
   @Test
   public void testSaveAppointmentSuccess() {
-    new Expectations() {{
-      repository.save(getGenericAppointment());
-      result = getGenericAppointment();
-    }};
-
     Appointment expected = new Appointment(
       "David Sweeney",
       "TEST_CONFIRMATION_NUMBER",
@@ -37,40 +25,14 @@ public class AppointmentControllerTest {
       "2021-04-09 21:00:00"
     );
 
-    expected = appointmentController.saveAppointment(expected);
-    Assertions.assertEquals(expected.getConfirmationNumber(),
-      appointmentController.saveAppointment(expected).getConfirmationNumber());
-  }
-
-  @Test
-  public void testGetAppointmentConfirmationSuccess() {
-    new Expectations(){{
-      repository.getAppointmentConfirmation("TEST_CONFIRMATION_NUMBER",
-        "test@abc.com");
-      result = getGenericAppointment();
-    }};
-
-    Appointment expected = new Appointment(
-      "David Sweeney",
-      "TEST_CONFIRMATION_NUMBER",
-      "test@abc.com",
-      "CONFIRMED",
-      "2021-04-09 21:00:00"
-    );
-
-    Assertions.assertEquals(expected.getConfirmationNumber(),
-      appointmentController.getAppointmentConfirmation(expected.getConfirmationNumber(),
-        expected.getEmail()).getConfirmationNumber());
+    appointmentController.saveAppointment(expected);
+    var expectedConfirmation = expected.getConfirmationNumber();
+    var actualConfirmation = appointmentController.getAppointmentConfirmation(expectedConfirmation, expected.getEmail()).getConfirmationNumber();
+    Assertions.assertEquals(expectedConfirmation, actualConfirmation);
   }
 
   @Test
   public void testDeleteAppointmentSuccess() {
-    new Expectations(){{
-      repository.delete("TEST_CONFIRMATION_NUMBER",
-        "test@abc.com");
-      result = "Appointment Cancelled!";
-    }};
-
     Appointment expected = new Appointment(
       "David Sweeney",
       "TEST_CONFIRMATION_NUMBER",
@@ -86,17 +48,13 @@ public class AppointmentControllerTest {
 
   @Test
   public void testUpdateAppointmentSuccess() {
-    Appointment actual = new Appointment(
+    appointmentController.saveAppointment(new Appointment(
       "David Sweeney",
       "TEST_CONFIRMATION_NUMBER",
       "test@abc.com",
       "CONFIRMED",
       "2021-04-09 21:00:00"
-    );
-    new Expectations(){{
-      repository.update("TEST_CONFIRMATION_NUMBER", actual);
-      result = "Appointment Cancelled!";
-    }};
+    ));
 
     Appointment expected = new Appointment(
       "David Sweeney",
@@ -106,7 +64,7 @@ public class AppointmentControllerTest {
       "2021-04-09 21:00:00"
     );
 
-    Assertions.assertEquals("Appointment Cancelled!",
+    Assertions.assertEquals("TEST_CONFIRMATION_NUMBER",
       appointmentController.updateAppointment(expected.getConfirmationNumber(),
         expected));
   }
